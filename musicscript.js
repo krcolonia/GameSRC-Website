6a2f97d77bd74ee17e34c4e2d51d50b0721e73c5
Kurt Colonia <kurtrobinperezcolonia@gmail.com>
2024-08-28 16:48:16 +0800

    1st Commit

---
+++ musicscript.js
@@ -0,0 +1,6 @@
+function play() {
+document.getElementById('fade').play(); document.getElementById('logo').src='music_play.gif';
+}
+function pause() {
+document.getElementById('fade').pause(); document.getElementById('logo').src='music_stop.png';
+}
---
+++ order.js
@@ -0,0 +1,116 @@
+	function hl() {
+ 	var node = document.createElement("LI");
+ 	var textnode = document.createTextNode("Half-Life");
+ 	node.appendChild(textnode);
+ 	document.getElementById("gameList").appendChild(node);
+}
+	function hlopfor() {
+ 	var node = document.createElement("LI");
+ 	var textnode = document.createTextNode("Half-Life : Opposing Forces");
+ 	node.appendChild(textnode);
+ 	document.getElementById("gameList").appendChild(node);
+}
+	function hlbs() {
+ 	var node = document.createElement("LI");
+ 	var textnode = document.createTextNode("Half-Life : Blue Shift");
+ 	node.appendChild(textnode);
+ 	document.getElementById("gameList").appendChild(node);
+}
+	function hldec() {
+ 	var node = document.createElement("LI");
+ 	var textnode = document.createTextNode("Half-Life : Decay");
+ 	node.appendChild(textnode);
+ 	document.getElementById("gameList").appendChild(node);
+}
+	function hl2() {
+ 	var node = document.createElement("LI");
+ 	var textnode = document.createTextNode("Half-Life 2");
+ 	node.appendChild(textnode);
+ 	document.getElementById("gameList").appendChild(node);
+}
+	function doom16() {
+ 	var node = document.createElement("LI");
+ 	var textnode = document.createTextNode("DOOM (2016)");
+ 	node.appendChild(textnode);
+ 	document.getElementById("gameList").appendChild(node);
+}
+	function doom20() {
+ 	var node = document.createElement("LI");
+ 	var textnode = document.createTextNode("DOOM Eternal");
+ 	node.appendChild(textnode);
+ 	document.getElementById("gameList").appendChild(node);
+}
+	function doom20() {
+ 	var node = document.createElement("LI");
+ 	var textnode = document.createTextNode("DOOM Eternal");
+ 	node.appendChild(textnode);
+ 	document.getElementById("gameList").appendChild(node);
+}
+	function smario() {
+ 	var node = document.createElement("LI");
+ 	var textnode = document.createTextNode("Super Mario Bros");
+ 	node.appendChild(textnode);
+ 	document.getElementById("gameList").appendChild(node);
+}
+	function smashbro() {
+ 	var node = document.createElement("LI");
+ 	var textnode = document.createTextNode("Super Smash Bros Ultimate");
+ 	node.appendChild(textnode);
+ 	document.getElementById("gameList").appendChild(node);
+}
+	function sonic() {
+ 	var node = document.createElement("LI");
+ 	var textnode = document.createTextNode("Sonic Mania Plus");
+ 	node.appendChild(textnode);
+ 	document.getElementById("gameList").appendChild(node);
+}
+	function smariomaker() {
+ 	var node = document.createElement("LI");
+ 	var textnode = document.createTextNode("Super Mario Maker");
+ 	node.appendChild(textnode);
+ 	document.getElementById("gameList").appendChild(node);
+}
+	function mc() {
+ 	var node = document.createElement("LI");
+ 	var textnode = document.createTextNode("Minecraft");
+ 	node.appendChild(textnode);
+ 	document.getElementById("gameList").appendChild(node);
+}
+	function terraria() {
+ 	var node = document.createElement("LI");
+ 	var textnode = document.createTextNode("Terraria");
+ 	node.appendChild(textnode);
+ 	document.getElementById("gameList").appendChild(node);
+}
+	function gmod() {
+ 	var node = document.createElement("LI");
+ 	var textnode = document.createTextNode("Garry's Mod");
+ 	node.appendChild(textnode);
+ 	document.getElementById("gameList").appendChild(node);
+}
+	function acnh() {
+ 	var node = document.createElement("LI");
+ 	var textnode = document.createTextNode("Animal Crossing : New Horizons");
+ 	node.appendChild(textnode);
+ 	document.getElementById("gameList").appendChild(node);
+}
+	function citiesky() {
+ 	var node = document.createElement("LI");
+ 	var textnode = document.createTextNode("Cities : Skylines");
+ 	node.appendChild(textnode);
+ 	document.getElementById("gameList").appendChild(node);
+}
+	function unisand() {
+ 	var node = document.createElement("LI");
+ 	var textnode = document.createTextNode("Universe Sandbox²");
+ 	node.appendChild(textnode);
+ 	document.getElementById("gameList").appendChild(node);
+}
+	function placeorder() {
+	var txt;
+	if (confirm("Place your order now?")) {
+	window.open("ordersucc.html", "_self");
+	} else {
+	txt = "You pressed Cancel.";
+	}
+}
